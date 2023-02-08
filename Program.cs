48902a6be5bd9bc50d6e5573383eab57426342a3
Adalmiina <adalmiinasuikkanen3@gmail.com>
2023-02-08 13:33:36 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("Hello, World!");
+                        
+
+        }
+    }
+}
